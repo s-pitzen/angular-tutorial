1885592ae270489926104a53fa920de604b3cb10
s-pitzen <100031645+s-pitzen@users.noreply.github.com>
2023-03-22 09:07:40 +0100

    2 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -24,7 +24,7 @@ export class CartComponent {
   onSubmit(): void {
     // Process checkout date here
     this.items = this.cartService.clearCart();
-    console.warn('Your order has been submitted', this.checkoutForm.value);
+    console.warn('Your order has been submitted ', this.checkoutForm.value);
     this.checkoutForm.reset();
   }
 }
